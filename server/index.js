8575b3f8c5b0c75e4884f98166169e38c607ea9c
quocthanhp <phamquocthanh1502@gmail.com>
2022-07-12 18:30:11 +1000

    make it better

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req,res) => {
     res.send('Hello to Memories API')
 })
 
-const PORT = process.env.PORT || 5000 // when push to heroku, heroku populates env PORT
+const PORT = process.env.PORT // when push to heroku, heroku populates env PORT
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
